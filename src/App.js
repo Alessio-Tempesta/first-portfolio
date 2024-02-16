42f5fcfa9ef196f56c9759e8ee4465512638b23f
Alessio-Tempesta <aletem999@gmail.com>
2024-02-16 22:10:36 +0100

    Create cartelle componenti,assets + iniziata e finita Navbar ed iniziata La Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import NavBar from "./components/NavBar";
+
 
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold underline font-signature">
-      Hello world!
-    </h1>
+    <NavBar />
     </div>
   );
 }
