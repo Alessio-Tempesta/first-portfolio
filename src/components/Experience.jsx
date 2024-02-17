b8affa26642059b739494d10f0c40eada24cf326
Alessio-Tempesta <aletem999@gmail.com>
2024-02-17 19:50:45 +0100

    aggiunte experience da completare

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div>
+
+        <div>
+            <div>
+                <p>Experience</p>
+                <p>Tecnologie con cui lavoro:</p>
+            </div>
+
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
